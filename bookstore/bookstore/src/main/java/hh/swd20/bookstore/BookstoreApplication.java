7a4d42b3341535aa07c9addfa2ad740aec7caace
elinapiispanen <elinapiispanen1@hotmail.fi>
2020-02-13 15:27:55 +0200

    jpa

--- bookstore/bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,13 +1,36 @@
 package hh.swd20.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			bookRepository.save(new Book("Otsikko", "Kirjoittaja", 2000, 19));
+			bookRepository.save(new Book("Toinen", "Kirjailija", 3030, 23));
+
+			log.info("fetch all cars");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
+	}
 }
--- bookstore/bookstore/src/main/java/hh/swd20/bookstore/domain/Book.java
+++ bookstore/bookstore/src/main/java/hh/swd20/bookstore/domain/Book.java
@@ -1,12 +1,29 @@
 package hh.swd20.bookstore.domain;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private int year;
 	private int isbn;
 
+	public Book(Long id, String title, String author, int year, int isbn) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+	}
+	
 	public Book(String title, String author, int year, int isbn) {
 		super();
 		this.title = title;
@@ -17,10 +34,14 @@ public class Book {
 
 	public Book() {
 		super();
+		this.id = null;
 		this.title = null;
 		this.author = null;
 		this.year = 0;
 		this.isbn = 0;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	
 	public String getTitle() {
@@ -29,6 +50,10 @@ public class Book {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	public Long getId() {
+		return id;
 	}
 
 	public String getAuthor() {
@@ -57,7 +82,8 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + "]";
 	}
+	
 
 }
